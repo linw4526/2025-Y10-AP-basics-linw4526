e67402ff6bb21f7045e3aa16bc5ea4b963a7618b
linw4526 <linw4526@masseyhigh.school.nz>
2025-07-30 09:12:18 +1200

    Add files via upload

---
+++ P_02_variables.py
@@ -0,0 +1,14 @@
+greeting = 'hello world'
+fav_num = 7
+love_programming = True
+print (greeting)
+print (fav_num)
+print (love_programming)
+my_name = 'SB'
+print (my_name)
+print (fav_num * 2)
+num_string = '5'
+print (num_string * 5)
+PI = 3.14
+a_01_my_num = 7
+

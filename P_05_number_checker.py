7917b1ded18c67e1fa30f7b91b5fcf8d0be5d3b4
linw4526 <linw4526@masseyhigh.school.nz>
2025-08-07 09:33:37 +1200

    Add files via upload

---
+++ P_05_number_checker.py
@@ -0,0 +1,18 @@
+def num_check (question):
+    error = "please enter a number that is more than zero\n"
+    while True:
+        try:
+            response = float(input(question))
+            if response > 0:
+                return response
+            else:
+                print (error)
+        except ValueError:
+            print (error)
+for item in range (0, 2):
+    width = num_check ('Width:')
+    print (width)
+print()
+for item in range (0, 2):
+    height = num_check ('Height:')
+    print (height)
---
+++ P_06_area_perim_calc.py
@@ -0,0 +1,7 @@
+width = (float(input('Width:')))
+height = (float(input('Height:')))
+area = height * width
+perimeter = 2 * (height + width)
+print()
+print (f'Perimeter: {perimeter} units')
+print (f'Area: {area} units')

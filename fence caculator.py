c7e8fd6951aac972bbbcce26148bd3f7317669a1
linw4526 <linw4526@masseyhigh.school.nz>
2025-08-28 11:57:53 +1200

    Add files via upload

---
+++ fence caculator.py
@@ -0,0 +1,26 @@
+def num_check (question):
+    error = "please enter a number that is more than zero\n"
+    while True:
+        try:
+            response = float(input(question))
+            if response > 0:
+                return response
+            else:
+                print (error)
+        except ValueError:
+            print (error)
+keep_going = ''
+while keep_going == '':
+    width = num_check ('Width in meter:')
+    height = num_check ('Height in meter:')
+    price = num_check('Price per meter:')
+    area = height * width
+    perimeter = 2 * (height + width)
+    totalprice = price * perimeter
+    print()
+    print(f'Perimeter: {perimeter:2f} meters')
+    print(f'Area: {area:2f} meters')
+    print(f'Price: {totalprice:2f} NZD')
+    keep_going = (input('Press enter to keep going or any key to quit'))
+    print()
+    print('Thanks')
